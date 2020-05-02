af9847669d31814f9b2dce07f8764a48c5787238
Ayman Sharaf <ayman.sharaf@echotechit.com>
2020-05-02 23:30:39 +0300

    Configure Application Entry Point

--- BackgroundServer.Client.ConsoleApplication/Program.cs
+++ BackgroundServer.Client.ConsoleApplication/Program.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace BackgroundServer.Client.ConsoleApplication
 {
     class Program
     {
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var environmentName = Environment.GetEnvironmentVariable("ENVIRONMENT");
+
+            var builder = new ContainerBuilder();
+
+            var hostBuilder = new HostBuilder()
+               .ConfigureAppConfiguration((hostContext, config) =>
+               {
+                   var builder = config
+                         .SetBasePath(AppDomain.CurrentDomain.BaseDirectory.ToString())
+                        .AddJsonFile($"appsettings.json", optional: false, reloadOnChange: true)
+                        .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+                        .AddEnvironmentVariables();
+
+                   if (environmentName.Equals("Development"))
+                   {
+                       builder.AddUserSecrets<Program>();
+                   }
+
+               }).UseServiceProviderFactory(new AutofacServiceProviderFactory())
+            .ConfigureContainer<ContainerBuilder>(builder =>
+            {
+                builder.RegisterAssemblyModules(typeof(Hangfire.BackgroundServer).Assembly);
+                builder.RegisterType<ProgramStarter>().SingleInstance();
+                builder.RegisterType<TopshelfStarter>().SingleInstance();
+            });
+
+            var container = builder.Build();
+            var host = hostBuilder.Build();
+
+            var programStarter = container.Resolve<ProgramStarter>();
+            programStarter.Start(host);
+
         }
     }
 }
---
+++ BackgroundServer.Client.ConsoleApplication/ProgramStarter.cs
@@ -0,0 +1,41 @@
+﻿using BackgroundServer.Abstractions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackgroundServer.Client.ConsoleApplication
+{
+    public class ProgramStarter
+    {
+        private readonly IBackgroundServerManager _backgroundServerManager;
+        private readonly IConfiguration _configuration;
+        private readonly TopshelfStarter _topshelfStarter;
+
+        public ProgramStarter(IBackgroundServerManager backgroundServerManager, IConfiguration configuration, TopshelfStarter topshelfStarter)
+        {
+            _backgroundServerManager = backgroundServerManager;
+            _configuration = configuration;
+            _topshelfStarter = topshelfStarter;
+        }
+
+        internal void Start(IHost host)
+        {
+            var topshelfEnabled = false;
+            bool.TryParse(_configuration["BackgroundServer:TopshelfEnabled"], out topshelfEnabled);
+
+            if (topshelfEnabled)
+            {
+                _topshelfStarter.Start(host);
+            }
+            else
+            {
+                _backgroundServerManager.Start(host.Services);
+            }
+
+        }
+    }
+}
