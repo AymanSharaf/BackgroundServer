b110076084b3560c56ca4a3aa048a721798d73d5
Ayman Sharaf <ayman.sharaf@echotechit.com>
2020-05-11 20:32:49 +0300

    Add Recurring and Self Starting jobs Scheduler interfaces

---
+++ BackgroundServer.Abstractions/Schedulers/IRecurringJobScheduler.cs
@@ -0,0 +1,15 @@
+﻿using BackgroundServer.Abstractions.Jobs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackgroundServer.Abstractions.Schedulers
+{
+    public interface IRecurringJobScheduler
+    {
+        void Register(IRecurringJob job);
+        void Remove(IRecurringJob job);
+    }
+}
---
+++ BackgroundServer.Abstractions/Schedulers/ISelfStartingJobsScheduler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackgroundServer.Abstractions.Schedulers
+{
+    public interface ISelfStartingJobsScheduler
+    {
+        void Inilialize();
+    }
+}
