ffd5a6623fac08d968457ac734b9e4a7d0a49a58
Ayman Sharaf <ayman.sharaf@echotechit.com>
2020-05-11 20:32:12 +0300

    Add jobs interfaces

---
+++ BackgroundServer.Abstractions/Jobs/IRecurringJob.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackgroundServer.Abstractions.Jobs
+{
+    public interface IRecurringJob: IJob
+    {
+        string CronExpression { get;}
+        void UpdateCronExpression(string expression);
+    }
+}
